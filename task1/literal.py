d2a9658c166eaf68144a702f4403746ee41af264
Nhung Bui <btcnhung.1299@gmail.com>
2019-12-04 15:15:43 +0700

    Update input from file

--- task1/literal.py
+++ task1/literal.py
@@ -2,7 +2,22 @@ class Literal:
    def __init__(self):
       self.negated = False
       self.symbol = ''
-      self.vars = []
 
+   @staticmethod
    def strpliteral(literal_str):
-      pass
+      literal_str = literal_str.strip()
+      literal = Literal()
+
+      if literal_str[0] == '-':
+         literal.negate()
+         literal.set_symbol(literal_str[1])
+      else:
+         literal.set_symbol(literal_str[0])
+
+      return literal
+
+   def negate(self):
+      self.negated = True
+   
+   def set_symbol(self, symbol):
+      self.symbol = symbol
--- task1/main.py
+++ task1/main.py
@@ -2,3 +2,6 @@ from knowledge_base import KnowledgeBase
 
 kb = KnowledgeBase()
 KnowledgeBase.declare(kb, '../test/01.txt')
+print('Knowledge base contains:')
+print('- Number of facts:', len(kb.facts))
+print('- Number of rules:', len(kb.rules))
