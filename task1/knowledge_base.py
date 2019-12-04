16d25ea60eb80e8a65a6b1c54efed2a4cb8197d2
Nhung Bui <btcnhung.1299@gmail.com>
2019-12-04 14:48:56 +0700

    Add main.py

--- task1/knowledge_base.py
+++ task1/knowledge_base.py
@@ -8,17 +8,26 @@ class KnowledgeBase:
    @staticmethod
    def declare(knowledge_base, path):
       with open(path, 'r') as f:
-         lines = f.readlines()
-         for clause_str in lines:
+         list_clause_str = f.readlines()
+
+         for clause_str in list_clause_str:
             clause = Clause.strpclause(clause_str)
             num_literals = clause.get_num_literals()
-            if num_literals = 1:
-               self.facts.append(clause)
+            if num_literals == 1:
+               knowledge_base.add_fact(clause)
             else:
-               self.rules.append(clause)
+               knowledge_base.add_rule(clause)
 
+   def add_fact(self, fact):
+      self.facts.append(fact)
+
+   def add_rule(self, rule):
+      self.rules.append(rule)
+
+   @staticmethod
    def tell():
       pass
 
+   @staticmethod
    def ask():
       pass
---
+++ task1/main.py
@@ -0,0 +1,4 @@
+from knowledge_base import KnowledgeBase
+
+kb = KnowledgeBase()
+KnowledgeBase.declare(kb, '../test/01.txt')
